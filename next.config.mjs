c486fb7936304053573585a103eaaffbc0bc4047
Onlook <git@onlook.com>
2025-06-27 16:00:35 -0700

    I want users to be able to add/edit data

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
 };
 export default nextConfig;
