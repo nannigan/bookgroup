563d0ac3e63506bd81386b1f63b0566778edb747
Nan Davenport <nannigan@nannigans.com>
2025-06-30 12:08:11 -0700

    fix localhost by making basepath conditional

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 import path from 'path';
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
-    basePath: '/bookgroup',
-    assetPrefix: '/bookgroup/',
+    basePath: isProd ? '/bookgroup' : '',
+    assetPrefix: isProd ? '/bookgroup/' : '',
     images: {
         unoptimized: true,
     },
